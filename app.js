caddd2e6a38f97b31070450fe96fa6ab38272cf5
Ashade Samson <ashadesamson99@gmail.com>
2024-04-17 08:01:43 +0100

    added the cookie parser and jwt pkgs

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express, { json } from "express";
 import { PORT } from "./config/config.js";
 import { connectToDatabase } from "./db/mongodb.js";
 import authRouter from "./routes/auth.js";
+import cookieParser from "cookie-parser";
 
 const app = express()
 connectToDatabase()
 
 // Middlewares
 app.use(express.json())
+app.use(cookieParser())
 
 
 // Routes
