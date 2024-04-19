ee76ff8d3fad07f3df55c96f5e4a97fe98bf3eaf
Ashade Samson <ashadesamson99@gmail.com>
2024-04-19 07:43:13 +0100

    updated the errorHandler to catch signin error

--- errorHandler/authError.js
+++ errorHandler/authError.js
@@ -2,6 +2,14 @@ export const authError = (err) => {
 
     let errors = { username: '', email: '', password: '' };
 
+    if (err.message === 'incorrect email') {
+      errors.email = 'That email is not registered';
+    }
+
+    if (err.message === 'incorrect password') {
+      errors.password = 'That password is incorrect';
+    }
+
     if (err.code === 11000) {
         errors = { code: "username or email is already in use"};
         return errors;
