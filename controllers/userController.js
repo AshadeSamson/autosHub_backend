4eb87772bf4cf418f8a132de3d812508c541c665
Ashade Samson <ashadesamson99@gmail.com>
2024-05-11 16:25:10 +0100

    updated the user update function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,24 @@
 import userModel from "../model/user.js";
+import CryptoJS from "crypto-js";
+import { PASS } from "../config/config.js";
 
 
 export const update = async (req, res) => {
 
+        // hash user password before update
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, PASS).toString()
+    }
+
     try {
-        res.status(200).json("you are about to update your account")
+        const updatedUser = await userModel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true})
+        const { password, ...userdetails} = updatedUser._doc
+        res.status(200).json({id: userdetails._id, email: userdetails.email, username: userdetails.username})
         
     } catch (error) {
 
-        res.status(400).json(error.message)
+        res.status(500).json(error.message)
+        console.log(error)
         
     }
 
