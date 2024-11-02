99fdc60051718b521dc9302777538d3becc85b12
Ashade Samson <ashadesamson99@gmail.com>
2024-11-02 08:39:02 +0100

    [refactor]: Add source folder to wrap the server logic

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import cors from "cors"
 import { PORT } from "./config/config.js";
 import { connectToDatabase } from "./db/mongodb.js";
